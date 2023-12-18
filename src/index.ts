3e4f3a46e48f4c64b9ac361d03bf86f766f967b8
Adam Rusted <hello@adamrusted.me>
2023-12-18 15:36:56 +0000

    Testing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
 const app = (req: VercelRequest, res: VercelResponse) => {
-  const items: number = req.statusCode || 0
-  return res.send(items)
+  const json = {
+    "status": "Winner, Winner"
+  }
+  return res.status(200).send(JSON.stringify(json))
 };
 
 export default app;
